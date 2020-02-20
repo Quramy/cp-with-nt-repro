b43f6d87bb49fe638eb41ccd3ab55b55303d55a6
Yosuke Kurami <yosuke.kurami@gmail.com>
2020-02-21 01:39:43 +0900

    show error

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const cp = require('child_process');
 
 const out = cp.spawnSync('npm', ['--version']);
+if (out.error) {
+  console.error(out.error);
+  process.exit(1);
+}
 console.log(out.stdout.toString('utf8'));
